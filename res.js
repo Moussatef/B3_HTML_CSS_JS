ab74c94717703709f4b7a7b3fa2bb1d65c63c67a
Moussatef <otman.moussetaf@gmail.com>
2021-02-14 01:29:41 +0100

    modification on HTML et JS

--- res.js
+++ res.js
@@ -8,15 +8,22 @@ let select_boit = document.getElementById("boit");
 function check_type_v(){
     var veh =document.getElementById("vehicule").value;
     switch(veh){
-        case "Motos" : select_carbu.innerHTML = `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+        case "Motos" : select_carbu.innerHTML += `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+                       
         break
-        case "Citadine" : select_carbu.innerHTML = `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+        case "Citadine" : 
+        for(let i=0;i<4;i++){
+            select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
+        }
+        
+        select_boit .innerHTML = `<option value="${boite[0]}">${boite[0]} </option>`;
+        break;
 
         default:
-            select_carbu.innerHTML = " <option >No value found</option>";
+            select_carbu.innerHTML = " <option >-Votre choix-</option>";
 
     }
-    alert(veh);
+    
 }
 
 
