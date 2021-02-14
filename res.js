23a531e3ade92b3953e44dc7ccf9508bc328b006
Moussatef <otman.moussetaf@gmail.com>
2021-02-14 03:23:40 +0100

    modification on HTML et JS

--- res.js
+++ res.js
@@ -2,8 +2,11 @@ let carburant = ["Electrique", "Hybride", "Essence", "Diesel"];
 let boite = ["Manuelle", "Automatique"];
 // var type_V = document.querySelector(".vehicule").value;
 let select_carbu = document.getElementById("carbur");
-let select_boit = document.getElementById("boit");
-let prix_pors = 0;
+let select_boit  = document.getElementById("boit");
+let prix_pors    = 0 ;
+let prix_voit    = 0 ;
+let pors_carbu   = 0 ;
+let pors_boit    = 0 ;
 
 
 function check_type_v() {
@@ -14,7 +17,7 @@ function check_type_v() {
             select_carbu.innerHTML = choix;
             select_carbu.innerHTML += `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
             select_boit.innerHTML = `<option value="None">None</option>`;
-            
+            prix_voit=10;
             break
         case "Citadine":
             select_carbu.innerHTML = choix;
@@ -22,6 +25,7 @@ function check_type_v() {
                 select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
             }
             select_boit.innerHTML = choix + `<option value="${boite[0]}">${boite[0]} </option>`;
+            prix_voit=12;
 
             break;
         case "Compact":
@@ -30,6 +34,7 @@ function check_type_v() {
                 select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
             }
             select_boit.innerHTML = choix + `<option value="${boite[0]}">${boite[0]} </option>`;
+            prix_voit=14;
             break;
         case "Berline":
             select_carbu.innerHTML = choix;
@@ -37,14 +42,16 @@ function check_type_v() {
                 select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
             }
             select_boit.innerHTML = choix + `<option value="${boite[1]}">${boite[1]} </option>`;
-            prix_pors = 20 + (20*0.19);
+            prix_voit=20;
+            
             break;
         case "Utilitare":
 
             select_carbu.innerHTML = choix + `<option value="${carburant[3]}">${carburant[3]} </option>`;
 
             select_boit.innerHTML = choix + `<option value="${boite[0]}">${boite[0]} </option>`;
-            prix_pors = 16 + (16*0.19);
+            
+            prix_voit=16;
             break;
         case "Engin":
             select_carbu.innerHTML = choix;
@@ -52,11 +59,13 @@ function check_type_v() {
                 select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
             }
             select_boit.innerHTML = choix + `<option value="${boite[0]}">${boite[0]} </option>`;
+            prix_voit=900;
             break;
         case "Camion":
             select_carbu.innerHTML = choix + `<option value="${carburant[3]}">${carburant[3]} </option>`;
             select_boit.innerHTML = choix + `<option value="${boite[1]}">${boite[1]} </option>`;
-            prix_pors = 250 + (250*0.40);
+            
+            prix_voit=250;
             break;
 
         default:
@@ -65,7 +74,39 @@ function check_type_v() {
     }
 }
 
+function check_carbu(){
+     switch(select_carbu){
+         case "Electrique" : pors_carbu = 0.05; break
+         case "Hybride" : pors_carbu = 0.09; break
+         case "Essence" : pors_carbu = 0.14; break
+         case "Diesel" : pors_carbu = 0.21; break
+         default: pors_carbu = 0;
+     }
+}
+function check_boit(){
+    switch(select_boit){
+        case "Manuelle" : pors_boit = 0; break
+        case "Automatique" : pors_boit = 0.19; break
+        default: pors_boit = 0;
+    }
+}
 
-check_type_v();
+function getduri(){
+    let date_Dep = document.getElementById("dateDep").value;
+    let date_Fin = document.getElementById("dateFin").value;
+    // alert(date_Dep +"     <<<>>>  "+ date_Dep);
 
-type_V.addEventListener("change", check_type_v);
+    let deffDate = new Date(date_Fin) - new Date(date_Dep);
+    const diffInDays = deffDate / (1000 * 60 * 60 * 24);
+    alert( diffInDays +"<>"+ prix_voit +"<>"+ prix_voit +"<>"+ pors_carbu +"<>"+ pors_boit);
+    
+    prix_pors = diffInDay * (prix_voit + (prix_voit*pors_carbu) + (prix_voit*pors_boit));
+    
+    alert("  durée de réservation (en jours) est :  "+ diffInDays );
+
+
+}
+
+select_carbu.addEventListener("change",check_carbu);
+select_boit.addEventListener("change",check_boit);
+document.getElementById("valider").addEventListener("click",getduri);
