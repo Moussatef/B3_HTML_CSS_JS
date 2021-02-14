4f04ba80f814de6429da1452d23880065286e76e
Moussatef <otman.moussetaf@gmail.com>
2021-02-14 01:17:15 +0100

    modification on HTML et JS

--- res.js
+++ res.js
@@ -1,17 +1,24 @@
 let carburant = ["Electrique","Hybride","Essence","Diesel"];
 let boite = ["Manuelle","Automatique"];
-let type_V = document.getElementById("vehicule");
+// var type_V = document.querySelector(".vehicule").value;
 let select_carbu = document.getElementById("carbur");
+let select_boit = document.getElementById("boit");
 
 
-function check_type_v(type_Vh){
-    switch(type_Vh){
-        case "Motos" : select_carbu.innerHTML = 
-        `
-        <option value="`+carburant[0]+`">`+carburant[0]+`</option>
-        <option value="`+carburant[2]+`">`+carburant[2]+`</option>
-        `
+function check_type_v(){
+    var veh =document.getElementById("vehicule").value;
+    switch(veh){
+        case "Motos" : select_carbu.innerHTML = `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+        break
+        case "Citadine" : select_carbu.innerHTML = `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+
+        default:
+            select_carbu.innerHTML = " <option >No value found</option>";
+
     }
+    alert(veh);
 }
-check_type_v(type_V.value);
-type_V.addEventListener("change",check_type_v)
+
+
+check_type_v();
+type_V.addEventListener("change",check_type_v);
