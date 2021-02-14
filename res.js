975321846f028c7ace9515ebf67339fddabb6122
Moussatef <otman.moussetaf@gmail.com>
2021-02-14 01:47:04 +0100

    modification on HTML et JS

--- res.js
+++ res.js
@@ -6,26 +6,56 @@ let select_boit = document.getElementById("boit");
 
 
 function check_type_v(){
+    let choix=" <option >-Votre choix-</option>";
     var veh =document.getElementById("vehicule").value;
     switch(veh){
-        case "Motos" : select_carbu.innerHTML += `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
+        case "Motos" :
+            select_carbu.innerHTML =choix;    
+            select_carbu.innerHTML += `<option value="${carburant[0]}">${carburant[0]} </option> <option value="${carburant[2]}">${carburant[2]}</option>"`;
                        
         break
         case "Citadine" : 
+        select_carbu.innerHTML =choix;
         for(let i=0;i<4;i++){
             select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
         }
-        select_boit .innerHTML = `<option value="${boite[0]}">${boite[0]} </option>`;
+        select_boit .innerHTML = choix+`<option value="${boite[0]}">${boite[0]} </option>`;
         break;
         case "Compact" : 
+        select_carbu.innerHTML =choix;
         for(let i=1;i<4;i++){
             select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
         }
-        select_boit .innerHTML = `<option value="${boite[0]}">${boite[0]} </option>`;
+        select_boit .innerHTML =choix+ `<option value="${boite[0]}">${boite[0]} </option>`;
+        break;
+        case "Berline" : 
+        select_carbu.innerHTML = choix;
+        for(let i=1;i<4;i++){
+            select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
+        }
+        select_boit .innerHTML = choix+`<option value="${boite[1]}">${boite[1]} </option>`;
+        break;
+        case "Utilitare" :
+
+            select_carbu.innerHTML =choix+ `<option value="${carburant[3]}">${carburant[3]} </option>`;
+        
+        select_boit .innerHTML =choix+ `<option value="${boite[0]}">${boite[0]} </option>`;
+        break;
+        case "Engin" : 
+        select_carbu.innerHTML = choix;
+        for(let i=2;i<4;i++){
+            select_carbu.innerHTML += `<option value="${carburant[i]}">${carburant[i]} </option>`;
+        }
+        select_boit .innerHTML =choix+ `<option value="${boite[0]}">${boite[0]} </option>`;
+        break;
+        case "Camion" :
+        select_carbu.innerHTML = choix+`<option value="${carburant[3]}">${carburant[3]} </option>`;
+        select_boit .innerHTML = choix+ `<option value="${boite[1]}">${boite[1]} </option>`;
         break;
 
+
         default:
-            select_carbu.innerHTML = " <option >-Votre choix-</option>";
+            select_carbu.innerHTML = choix;
 
     }
     
