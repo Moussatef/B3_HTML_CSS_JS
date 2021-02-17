cd5064e92becd11d0daefab2977c7d17f6cb59ac
Moussatef <otman.moussetaf@gmail.com>
2021-02-17 09:28:06 +0100

    modification on JS

--- res.js
+++ res.js
@@ -1,4 +1,14 @@
 let carburant = ["Electrique", "Hybride", "Essence", "Diesel"];
+var type_V={
+    "Motos":["Electrique","Essence"],
+    "Citadine":["Electrique", "Hybride", "Essence", "Diesel"],
+    "Compact": ["Hybride", "Essence", "Diesel"],
+    "Berline": ["Essence", "Diesel"],
+    "Utilitare":["Diesel"],
+    "Engin" :["Essence", "Diesel"],
+    "Camion" :["Diesel"]
+
+}
 let boite = ["Manuelle", "Automatique"];
 // var type_V = document.querySelector(".vehicule").value;
 let select_carbu = document.getElementById("carbur");
@@ -7,6 +17,7 @@ var prix_pors;
 var prix_voit;
 var pors_carbu;
 var pors_boit =0;
+document.getElementById("btn_ok").style.display="none"
 
 
 function check_type_v() {
@@ -90,7 +101,8 @@ function check_boit() {
    
 }
 let propt1 = document.getElementById("propt1");
-    let propt2 = document.getElementById("propt2");
+let propt2 = document.getElementById("propt2");
+
 function getduri() {
     let date_Dep = document.getElementById("dateDep").value;
     let date_Fin = document.getElementById("dateFin").value;
@@ -98,18 +110,17 @@ function getduri() {
     let email = document.getElementById("email").value;
     let CIN = document.getElementById("cin").value;
     let typeca = document.getElementById("vehicule").value;
-    
-    // alert(date_Dep +"     <<<>>>  "+ date_Dep);
+    propt1.style.display="block";
+    document.getElementById("btn_ok").style.display="block";
     let deffDate = new Date(date_Fin) - new Date(date_Dep);
     let diffInDays = deffDate / (1000 * 60 * 60 * 24);
-    // alert( diffInDays +"<>"+ prix_voit +"<>"+ pors_carbu +"<>"+ pors_boit);
     prix_pors = diffInDays * (prix_voit + (prix_voit * pors_carbu) + (prix_voit * pors_boit));
     propt1.className="prop";
     propt2.innerHTML = "<p> Nom Et Prenom :  " + Nom + " <br/>CIN : " + CIN + "<br/>  Email : " + email+"<br/> Type du véhicule : " + typeca + "<br/> carburant : " + select_carbu.value + "<br/> boite à vitesse : " + select_boit.value + " <br/>durée de réservation (en jours) est :  " + diffInDays + "<br/>prix total est :  " + prix_pors + "€ <p> "
-    propt1.innerHTML +=`<div id="btn_ok" class="button_hero" style="float: right; margin-right: 5%;"> <a class="btn_rm reser" href="#">OK</a> </div>`;
+    
 }
 function ok(){
-    propt1.className("prop1");
+    propt1.style.display="none";
 }
 
 select_carbu.addEventListener("change", check_carbu);
